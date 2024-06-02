f0cf1a4a3142c7ba17740c1df6017a40d34430f3
Ingrid <dias.ingridsouza@gmail.com>
2024-06-02 20:32:02 -0300

    Blog Pessoal Frontend

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-﻿import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+﻿import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 import { ReactNode, useContext } from 'react'
 import { AuthContext } from '../../contexts/AuthContext'
 
@@ -17,18 +17,18 @@ function Footer() {
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                        Blog Pessoal Generation | Copyright: {data}
+                        Blog Pessoal Ingrid Dias | Copyright: {data}
                     </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <a href="https://www.linkedin.com/school/generationbrasil" target="_blank">
+                        <a href="www.linkedin.com/in/ingrid-dias-souza" target="_blank">
                             <LinkedinLogo size={48} weight='bold' />
                         </a>
-                        <a href="https://www.instagram.com/generationbrasil" target="_blank">
+                        <a href="https://www.instagram.com" target="_blank">
                             <InstagramLogo size={48} weight='bold' />
                         </a>
-                        <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                            <FacebookLogo size={48} weight='bold' />
+                        <a href="https://github.com/ingrid-dias" target="_blank">
+                            <GithubLogo size={48} weight='bold' />
                         </a>
                     </div>
                 </div>
